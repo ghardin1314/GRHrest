652fd345d179af7465e5f7371e5fa764c6ce07e2
ghardin1314 <g.hardin1314@gmail.com>
2020-05-03 12:53:16 -0700

    moved settings to keys.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -108,8 +108,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'HOST': '127.0.0.1',
         'PORT': '3306',
-        'NAME': 'grhlocaldb',
-        'USER': 'garrett',
+        'NAME': db_name,
+        'USER': username,
         'PASSWORD': password,
 
     }
